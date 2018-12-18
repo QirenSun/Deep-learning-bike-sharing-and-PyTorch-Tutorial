9b9423187fbb614293d92d4ac09e2b0af5840fc3
Qiren Sun <sun18033802009@gmail.com>
2018-12-17 22:24:48 -0500

    Update cifar10_cnn.py

--- PyTorch Tutorial/Build_Convolutional_Layers/cifar10_cnn.py
+++ PyTorch Tutorial/Build_Convolutional_Layers/cifar10_cnn.py
@@ -50,6 +50,104 @@ for idx in np.arange(20):
     imshow(images[idx])
     ax.set_title(classes[labels[idx]])
 
+import torch.nn as nn
+import torch.nn.functional as F
+
+# define the CNN architecture
+class Net(nn.Module):
+    def __init__(self):
+        super(Net, self).__init__()
+        # convolutional layer 
+        self.conv1 = nn.Conv2d(3, 16, 3, padding=1)
+        self.conv2 = nn.Conv2d(16, 32, 3, padding=1)
+        self.conv3 = nn.Conv2d(32, 64, 3, padding=1)
+        # max pooling layer
+        self.pool = nn.MaxPool2d(2, 2)     
+        self.fc1 = nn.Linear(64 * 4 * 4, 500)
+        self.fc2 = nn.Linear(500, 10)
+        self.dropout = nn.Dropout(0.25)
+
+    def forward(self, x):
+        # add sequence of convolutional and max pooling layers
+        x = self.pool(F.relu(self.conv1(x)))
+        x = self.pool(F.relu(self.conv2(x)))
+        x = self.pool(F.relu(self.conv3(x)))
+        # flatten image input
+        x = x.view(-1, 64 * 4 * 4)
+        x = self.dropout(x)
+        # add 1st hidden layer
+        x = F.relu(self.fc1(x))
+        # add dropout layer
+        x = self.dropout(x)
+        # add 2nd hidden layer
+        x = self.fc2(x)
+        return x
+
+# create a complete CNN
+model = Net()
+print(model)
+
+import torch.optim as optim
+
+criterion = nn.CrossEntropyLoss()
+optimizer = optim.SGD(model.parameters(), lr=0.01)
+
+# number of epochs to train the model
+n_epochs = 30
+
+valid_loss_min = np.Inf # track change in validation loss
+
+for epoch in range(1, n_epochs+1):
+
+    # keep track of training and validation loss
+    train_loss = 0.0
+    valid_loss = 0.0
+    
+  
+    # train the model 
+    model.train()
+    for data, target in train_loader:
+        optimizer.zero_grad()
+        # forward pass: compute predicted outputs by passing inputs to the model
+        output = model(data)
+        # calculate the batch loss
+        loss = criterion(output, target)
+        # backward pass: compute gradient of the loss with respect to model parameters
+        loss.backward()
+        # perform a single optimization step (parameter update)
+        optimizer.step()
+        # update training loss
+        train_loss += loss.item()*data.size(0)
+        
+  
+    # validate the model 
+    model.eval()
+    for data, target in valid_loader:
+        # move tensors to GPU if CUDA is available
+        if train_on_gpu:
+            data, target = data.cuda(), target.cuda()
+        # forward pass: compute predicted outputs by passing inputs to the model
+        output = model(data)
+        # calculate the batch loss
+        loss = criterion(output, target)
+        # update average validation loss 
+        valid_loss += loss.item()*data.size(0)
+    
+    # calculate average losses
+    train_loss = train_loss/len(train_loader.dataset)
+    valid_loss = valid_loss/len(valid_loader.dataset)
+        
+    # print training/validation statistics 
+    print('Epoch: {} \tTraining Loss: {:.6f} \tValidation Loss: {:.6f}'.format(
+        epoch, train_loss, valid_loss))
+    
+    # save model if validation loss has decreased
+    if valid_loss <= valid_loss_min:
+        print('Validation loss decreased ({:.6f} --> {:.6f}).  Saving model ...'.format(valid_loss_min,valid_loss))
+        torch.save(model.state_dict(), 'model_cifar.pt')
+        valid_loss_min = valid_loss
+
+
 
 
 
